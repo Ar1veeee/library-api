2d6626093a959c35e1d0d294556a261e438f2535
Ar1veeee <aliefarifin99@gmail.com>
2025-12-27 06:30:20 +0700

    error: fixing error sql syntax

--- internal/repository/member_repository.go
+++ internal/repository/member_repository.go
@@ -17,7 +17,7 @@ func NewMemberRepository(db *sql.DB) *MemberRepository {
 }
 
 func (r *MemberRepository) GetByID(ctx context.Context, memberID int) (*model.Member, error) {
-	query := `SELECT id, name, email, FROM members WHERE id = ?`
+	query := `SELECT id, name, email FROM members WHERE id = ?`
 
 	var member model.Member
 	err := r.db.QueryRowContext(ctx, query, memberID).Scan(
